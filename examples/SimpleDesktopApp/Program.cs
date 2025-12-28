8842535ce43a33832c0bffbb4fa8180b5bc337aa
Xenthio <thegooster88@gmail.com>
2025-12-29 00:25:37 +1100

    Update Program.cs

--- examples/SimpleDesktopApp/Program.cs
+++ examples/SimpleDesktopApp/Program.cs
@@ -9,9 +9,9 @@ class Program
     {
         // Run the application with FlexboxTest to verify layout (colored divs)
         // Switch to MainApp to test text rendering
-        Avalazor.UI.AvalazorApplication.RunPanel<FlexboxTest>(title: "Avalazor - Flexbox Layout Test");
+        // Avalazor.UI.AvalazorApplication.RunPanel<FlexboxTest>(title: "Avalazor - Flexbox Layout Test");
         
         // Or use MainApp with text:
-        // Avalazor.UI.AvalazorApplication.RunPanel<MainApp>(title: "Avalazor - Desktop Razor with XGUI Themes");
+        Avalazor.UI.AvalazorApplication.RunPanel<MainApp>(title: "Avalazor - Desktop Razor with XGUI Themes");
     }
 }
