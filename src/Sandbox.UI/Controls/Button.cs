0cd2452e5878238bb08a16f76d017375e65725d6
Xenthio <thegooster88@gmail.com>
2025-12-31 19:45:27 +1100

    Update Button.cs

--- src/Sandbox.UI/Controls/Button.cs
+++ src/Sandbox.UI/Controls/Button.cs
@@ -1,41 +1,53 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+//using Sandbox.UI.Navigation;
+
 namespace Sandbox.UI;
 
 /// <summary>
-/// A simple button panel.
-/// Based on s&box's Button.
+/// A simple button <see cref="Panel"/>.
 /// </summary>
-[Library("button")]
-public class Button : Panel
+[Library("Button")]
+public class Button : Panel //, INavigationEvent
 {
     /// <summary>
-    /// The Label that displays Text.
+    /// The <see cref="Label"/> that displays <see cref="Text"/>.
     /// </summary>
-    protected Label? TextLabel;
+    protected Label TextLabel;
 
     /// <summary>
-    /// The IconPanel that displays Icon.
+    /// The <see cref="IconPanel"/> that displays <see cref="Icon"/>.
     /// </summary>
-    protected IconPanel? IconPanel;
+    protected IconPanel IconPanel;
 
     /// <summary>
-    /// The Label that displays Help.
+    /// The <see cref="Label"/> that displays <see cref="Help"/>.
     /// </summary>
-    protected Label? HelpLabel;
+    protected Label HelpLabel;
 
     /// <summary>
     /// The column on the right, holding the label and help
     /// </summary>
-    protected Panel? RightColumn;
+    protected Panel RightColumn;
+
+    /// <summary>
+    /// The target link, if set
+    /// </summary>
+    [Parameter] public string Href { get; set; }
 
     /// <summary>
     /// Used for selection status in things like ButtonGroup
     /// </summary>
-    public virtual object? Value { get; set; }
+    public virtual object Value { get; set; }
+
+    /// <summary>
+    /// More button options underneath the main button content.
+    /// </summary>
+    [Parameter] public RenderFragment HoverMenu { get; set; }
 
     public Button()
     {
         AddClass("button");
-        ElementName = "button";
 
         IconPanel = AddChild(new IconPanel("people", "icon"));
         IconPanel.Style.Display = DisplayMode.None;
@@ -46,11 +58,11 @@ public class Button : Panel
         TextLabel = RightColumn.AddChild(new Label("Empty Label", "button-label button-text"));
         TextLabel.Style.Display = DisplayMode.None;
 
-        HelpLabel = RightColumn.AddChild(new Label("", "button-help"));
+        HelpLabel ??= RightColumn.AddChild(new Label("", "button-help"));
         HelpLabel.Style.Display = DisplayMode.None;
     }
 
-    public Button(string? text, Action? action = default) : this()
+    public Button(string text, Action action = default) : this()
     {
         if (text != null)
             Text = text;
@@ -59,7 +71,7 @@ public class Button : Panel
             AddEventListener("onclick", action);
     }
 
-    public Button(string? text, string? icon) : this()
+    public Button(string text, string icon) : this()
     {
         if (icon != null)
             Icon = icon;
@@ -68,21 +80,10 @@ public class Button : Panel
             Text = text;
     }
 
-    public Button(string? text, string? icon, Action? onClick) : this(text, icon)
-    {
-        if (onClick != null)
-            AddEventListener("onclick", onClick);
-    }
-
-    public Button(string? text, string? icon, string? className, Action? onClick) : this(text, icon, onClick)
-    {
-        if (className != null)
-            AddClass(className);
-    }
-
     /// <summary>
     /// The button is disabled for some reason
     /// </summary>
+    [Parameter]
     public bool Disabled
     {
         get => HasClass("disabled");
@@ -92,18 +93,27 @@ public class Button : Panel
     /// <summary>
     /// Allow external factors to force the active state
     /// </summary>
-    public bool Active { get; set; }
+    [Parameter] public bool Active { get; set; }
+
+    public Button(string text, string icon, Action onClick) : this(text, icon)
+    {
+        AddEventListener("onclick", onClick);
+    }
+
+    public Button(string text, string icon, string className, Action onClick) : this(text, icon, onClick)
+    {
+        AddClass(className);
+    }
 
     /// <summary>
     /// Text for the button.
     /// </summary>
-    public string? Text
+    [Parameter]
+    public string Text
     {
         get => TextLabel?.Text;
         set
         {
-            if (TextLabel == null) return;
-
             if (string.IsNullOrEmpty(value))
             {
                 TextLabel.Style.Display = DisplayMode.None;
@@ -112,8 +122,7 @@ public class Button : Panel
             }
 
             TextLabel.Style.Display = DisplayMode.Flex;
-            if (RightColumn != null)
-                RightColumn.Style.Display = DisplayMode.Flex;
+            RightColumn.Style.Display = DisplayMode.Flex;
             TextLabel.Text = value;
         }
     }
@@ -121,13 +130,12 @@ public class Button : Panel
     /// <summary>
     /// Help for the button.
     /// </summary>
-    public string? Help
+    [Parameter]
+    public string Help
     {
         get => HelpLabel?.Text;
         set
         {
-            if (HelpLabel == null) return;
-
             if (string.IsNullOrEmpty(value))
             {
                 HelpLabel.Style.Display = DisplayMode.None;
@@ -136,22 +144,31 @@ public class Button : Panel
             }
 
             HelpLabel.Style.Display = DisplayMode.Flex;
-            if (RightColumn != null)
-                RightColumn.Style.Display = DisplayMode.Flex;
+            RightColumn.Style.Display = DisplayMode.Flex;
             HelpLabel.Text = value;
         }
     }
 
     /// <summary>
+    /// Deletes the <see cref="Text"/>.
+    /// </summary>
+    public void DeleteText()
+    {
+        if (!TextLabel.IsValid()) return;
+
+        TextLabel?.Delete();
+        TextLabel = null;
+    }
+
+    /// <summary>
     /// Icon for the button.
     /// </summary>
-    public string? Icon
+    [Parameter]
+    public string Icon
     {
         get => IconPanel?.Text;
         set
         {
-            if (IconPanel == null) return;
-
             if (string.IsNullOrEmpty(value))
             {
                 IconPanel.Style.Display = DisplayMode.None;
@@ -165,7 +182,15 @@ public class Button : Panel
     }
 
     /// <summary>
-    /// Set the text for the button.
+    /// Deletes the <see cref="Icon"/>.
+    /// </summary>
+    public void DeleteIcon()
+    {
+        IconPanel.Style.Display = DisplayMode.None;
+    }
+
+    /// <summary>
+    /// Set the text for the button. Calls <c>Text = value</c>
     /// </summary>
     public virtual void SetText(string text)
     {
@@ -185,30 +210,46 @@ public class Button : Panel
         switch (name)
         {
             case "text":
+                {
+                    SetText(value);
+                    return;
+                }
+
             case "html":
-                SetText(value);
-                return;
+                {
+                    SetText(value);
+                    return;
+                }
 
             case "icon":
-                Icon = value;
-                return;
+                {
+                    Icon = value;
+                    return;
+                }
+
+            case "href":
+                {
+                    Href = value;
+                    return;
+                }
 
             case "active":
-                SetClass("active", value == "true" || value == "1");
-                return;
-
-            case "disabled":
-                Disabled = value == "true" || value == "1";
-                return;
+                {
+                    SetClass("active", value == "true" || value == "1");
+                    return;
+                }
         }
 
         base.SetProperty(name, value);
     }
 
-    public override void SetContent(string? value)
+    public override void SetContent(string value)
     {
         SetText(value?.Trim() ?? "");
     }
+
+    //NavigationHost _navigatorCache;
+
 
     public override void Tick()
     {
@@ -218,6 +259,55 @@ public class Button : Panel
 
     protected void UpdateActiveState()
     {
-        SetClass("active", Active);
+        if (Active)
+        {
+            SetClass("active", true);
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(Href))
+        {
+            SetClass("active", false);
+            return;
+        }
+
+        //_navigatorCache ??= Ancestors.OfType<NavigationHost>().FirstOrDefault();
+
+        //var active = (_navigatorCache?.CurrentUrlMatches(Href) ?? false);
+        //SetClass("active", active);
     }
+
+    protected override void OnMouseDown(MousePanelEvent e)
+    {
+        base.OnMouseDown(e);
+
+        //if (!string.IsNullOrWhiteSpace(Href))
+        //{
+        //    this.Navigate(Href);
+        //    e.StopPropagation();
+        //}
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder tree)
+    {
+        if (HoverMenu != null)
+        {
+            var classes = "button-hover-menu";
+            if (!HasHovered) classes += " hidden";
+
+            tree.OpenElement<Panel>(0);
+            tree.AddAttribute(1, "class", classes);
+
+            HoverMenu?.Invoke(tree);
+            tree.CloseElement();
+        }
+    }
+
+    protected override int BuildHash() => HashCode.Combine(HoverMenu, HasHovered);
+    protected override string GetRenderTreeChecksum() => $"{BuildHash()}";
+
+    //void INavigationEvent.OnNavigated(string url)
+    //{
+    //    UpdateActiveState();
+    //}
 }
